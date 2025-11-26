18ef539887915382fa148fee702e28795f5c6e14
CatherineCharlesFesto <Catherinecharlesfesto@gmail.com>
2025-11-26 14:04:56 +0300

    love

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-import math
+class IntegerToRoman:
+    def __init__(self, number):
+        """Constructor: initializes the object with an integer value."""
+        self.number = number
 
-print(math.isclose(12.05,12.45))
-print(math.isclose(12.30,12.31))
+    def convert(self):
+        """Converts the stored integer to a Roman numeral."""
+        value_map = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+        ]
+
+        num = self.number
+        roman = ""
+
+        for value, symbol in value_map:
+            while num >= value:
+                roman += symbol
+                num -= value
+
+        return roman
+
+
+# --- Example Usage ---
+if __name__ == "__main__":
+    number = int(input("Enter an integer: "))
+    converter = IntegerToRoman(number)  # Creating an object
+    print("Roman numeral:", converter.convert())
